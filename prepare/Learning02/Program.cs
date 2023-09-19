0fd7b5ccb11760f67d7021e395bfef65ad28b1a9
Brock Lewis <blewis@sytechsolutions.com>
2023-09-19 15:14:32 -0600

    Added Job and Resume classes.

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Google";
+        job1._startYear = 2012;
+        job1._endYear = 2015;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Senior Software Engineer";
+        job2._company = "Twitter";
+        job2._startYear = 2015;
+        job2._endYear = 2016;
+
+        Resume resume1 = new Resume();
+        resume1._name = "Test Person";
+        resume1._jobs = new List<Job> {job1, job2}; 
+
+        resume1.Display();
     }
 }

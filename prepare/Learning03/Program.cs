b4c1ca9eced1b6cbd6e39c59abb8be313f72a00a
Brock Lewis <blewis@sytechsolutions.com>
2023-09-25 10:14:52 -0600

    Finished Learning03.

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction fraction1 = new Fraction();
+        Fraction fraction2 = new Fraction(6);
+        Fraction fraction3 = new Fraction(6, 7);
+        Console.WriteLine(fraction1.GetFractionString());
+        Console.WriteLine(fraction2.GetFractionString());
+        Console.WriteLine(fraction3.GetFractionString());
+
+        fraction1.SetTop(4);
+        fraction1.SetBottom(9);
+
+        Console.WriteLine($"Top: {fraction1.GetTop()}");
+        Console.WriteLine($"Bottom: {fraction1.GetBottom()}");
+
+        Fraction fraction4 = new Fraction();
+        Fraction fraction5 = new Fraction(5);
+        Fraction fraction6 = new Fraction(3, 4);
+        Fraction fraction7 = new Fraction(1, 3);
+
+        Console.WriteLine(fraction4.GetFractionString());
+        Console.WriteLine(fraction4.GetDecimalValue());
+        Console.WriteLine(fraction5.GetFractionString());
+        Console.WriteLine(fraction5.GetDecimalValue());
+        Console.WriteLine(fraction6.GetFractionString());
+        Console.WriteLine(fraction6.GetDecimalValue());
+        Console.WriteLine(fraction7.GetFractionString());
+        Console.WriteLine(fraction7.GetDecimalValue());
     }
 }

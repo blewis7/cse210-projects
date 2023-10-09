89d7d45422a70062ef1de92ba8b1541837dedcb3
Brock Lewis <blewis@sytechsolutions.com>
2023-10-08 20:01:48 -0600

    Finished Learning05

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square square = new Square("green", 5);
+        // Console.WriteLine($"Square: Color is {square.GetColor()}, and area is {square.GetArea()}");
+
+        Rectangle rectangle = new Rectangle("purple", 5, 4);
+        // Console.WriteLine($"Rectangle: Color is {rectangle.GetColor()}, and area is {rectangle.GetArea()}");
+
+        Circle circle = new Circle("red", 2);
+        // Console.WriteLine($"Circle: Color is {circle.GetColor()}, and area is {circle.GetArea()}");
+
+        List<Shape> shapes = new List<Shape>{ square, rectangle, circle };
+
+        foreach(Shape shape in shapes)
+        {
+            Console.WriteLine($"Color is {shape.GetColor()}, and area is {shape.GetArea()}");
+        }
     }
 }
